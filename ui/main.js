9a69ae27a14ca57cafbe824496af9a63558f72e6
ckarthikk <karthik87.ck@gmail.com>
2018-02-21 14:31:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var text = document.getElementById("maintext");
+text.innerHTML = "New Value";
+
+var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + "px";
+}
+img.onclick = function() {
+  var interval = setInterval(moveRight, 50);
+};
